97ed997a65bef693966d17f35c926746723826ad
Israr Akhundzada <133725332+israrakh@users.noreply.github.com>
2024-09-30 20:02:58 -0400

    Calculator two has been modified

--- Selection and Repetition Projects.cpp
+++ Selection and Repetition Projects.cpp
@@ -1,9 +1,44 @@
 // Selection and Repetition Projects.cpp : This file contains the Calculator and Merge Files programs.
+// Calculator program two. 
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-   cout << "This file contains the Calculator and Merge Files programs.";
+    // Declare and initialize variables.
+    int num1, num2;
+    char ch;
+
+    // Getting user input and printing the messages.
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> ch;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Checking and Performing operation based on the operator.
+    switch (ch) {
+
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0)
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+        else
+            cout << "Division by zero is not allowed!!";
+        break;
+    default:
+        cout << "Unsupported operation!";
+    }
+
+    return 0;
 }
