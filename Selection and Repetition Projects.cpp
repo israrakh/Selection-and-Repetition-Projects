711e840a13dbe2a0abee54eabcbe07ae9bebf6df
Israr <israr.akh03@gmail.com>
2024-09-30 21:41:44 -0400

    master has been updated

--- Selection and Repetition Projects.cpp
+++ Selection and Repetition Projects.cpp
@@ -1,16 +1,10 @@
 // Selection and Repetition Projects.cpp : This file contains the Calculator and Merge Files programs.
-
-
-// Merge Files Program
  
-#include <iostream>
-#include <fstream>
-#include <string>
 
+#include <iostream>
 using namespace std;
 
 int main()
 {
     cout << "This file contains the Calculator and Merge Files programs.";
-
 }
